bafa8015eb1ea773f1bbbb362ee342a49b84f462
Ben Johnson <ben@particle41.com>
2017-05-25 11:00:48 -0500

    retested regex for a different use case

--- lib/TextSearch.rb
+++ lib/TextSearch.rb
@@ -1,26 +1,27 @@
 
 
-class TextSearcher 
+class TextSearcher
 
 	# Initializes the text searcher with the contents of a text file.
-	# The current implementation just reads the contents into a string 
+	# The current implementation just reads the contents into a string
 	# and passes them to #init().  You may modify this implementation if you need to.
-	
-	attr_accessor :file 		
+
+	attr_accessor :file
 
 
 	def initialize(file)
-		@file = File.open(file) 
+		@file = File.open(file)
 	end
 
 	def searcher(word,context_count)
 		context_count || context_count = 0
 		rex = "A-Za-z0-9_'!$"
+		puts "(/(?:[#{rex}.]+[^#{rex}]+){0,#{context_count}}#{word}(?:[^#{rex}]+[#{rex}.]+){0,#{context_count}}/)"
 		@file.read.scan(/(?:[#{rex}.]+[^#{rex}]+){0,#{context_count}}#{word}(?:[^#{rex}]+[#{rex}.]+){0,#{context_count}}/)
-	end 
-	
+	end
 
-	
+
+
 # 	/**
 # 	 *  Initializes any internal data structures that are needed for
 # 	 *  this class to implement search efficiently.
@@ -28,9 +29,9 @@ class TextSearcher
 # 	protected void init(String fileContents) {
 # 		// TODO -- fill in implementation
 # 	}
-	
+
 # 	/**
-# 	 * 
+# 	 *
 # 	 * @param queryWord The word to search for in the file contents.
 # 	 * @param contextWords The number of words of context to provide on
 # 	 *                     each side of the query word.
